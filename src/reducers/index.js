33f3e7d1081ce3e53d93c79c8a6c931455d41832
Chris Williams <chriswilliams1977@gmail.com>
2017-09-27 15:46:02 +0200

    adding actions to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,13 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books'
 import ActiveBookReducer from './reducer_active_book'
 
-//these end up as keys on our global state
+//Reducers get combined here to create application state
+//This is completely seoerate from component state
+//For key in combineReducers we assign one reducer and that is responsible for creating one piece of state
+//For example ActiveBookReducer creates activeBook state
+//Reducers change state via actions
+//When action is dispatch it flows through all reducers
+//Via a switch each reducer has the option to return a new piece of state
 const rootReducer = combineReducers({
     books: BooksReducer,
     activeBook: ActiveBookReducer
